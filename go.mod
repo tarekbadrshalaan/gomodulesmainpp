45ee2002c652ab115a454c1647a9f75d484f18a9
tarekbadr <tarekbadrshalaan@gmail.com>
2018-10-02 15:21:38 +0200

    init mod

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tarekbadrshalaan/gomodulesmainpp
+
+require github.com/tarekbadrshalaan/gomodulesdepone v1.0.0
--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
-	//"github.com/tarekbadrshalaan/gomodulesdepone/v1"
+
+	"github.com/tarekbadrshalaan/gomodulesdepone"
 )
 
 func main() {
 	fmt.Println("Hi from Main app")
-	//gomodulesdepone.GetData()
+
+	gomodulesdepone.GetData()
 }
